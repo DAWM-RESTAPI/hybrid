2a250ed7a8e9ba596271bfe997f2d2cdaf9ad6c7
DanielMateoOrellana <107244270+DanielMateoOrellana@users.noreply.github.com>
2024-09-01 20:33:23 -0500

    More changes

--- src/app/lessons/lessons.page.ts
+++ src/app/lessons/lessons.page.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 
+/* 3. Importe del método http */
+import { HttpClientModule } from '@angular/common/http';
+
+/* 4. Importe de la interfaz */
+import { Data } from '../interfaces/data';
+
+/* 5. Importe del servicio */
+import { ProviderService } from '../services/provider.service';
+
+/* 6. Importe lo constructores de formulario */
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-lessons',
   templateUrl: './lessons.page.html',
@@ -9,10 +20,45 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [
     IonicModule,
+    CommonModule, HttpClientModule, ReactiveFormsModule,
     CommonModule
-  ]
+  ],
+  providers: [ProviderService]
 })
 export class LessonsPage {
+
+  public data : Data[] = [];
+  /* 10. Formulario reactivo */
+  checkoutForm = this.formBuilder.group({
+    texto: ''
+  });
+
+  constructor(private dataProvider: ProviderService , private formBuilder: FormBuilder) { }
+  
+  /* 12. Ejecución de la petición y suscripción de la respuesta */
+  ngOnInit() {
+    this.loadData()
+  }
+
+  loadData() {
+    this.dataProvider.getResponse().subscribe( response => {
+      console.log(response);
+      if( response != null) {
+        this.data = Object.values(response) as Data[]
+      }
+        
+    })
+  }
+
+  /* 13. Callback para el envío de datos */
+  onSubmit(): void {
+    this.dataProvider.postResponse(this.checkoutForm.value).subscribe( (response) => {
+            this.checkoutForm.reset();
+            this.loadData()
+    })
+  }
+  
+  
   lessons = [
     {
       id: 1,
@@ -34,3 +80,4 @@ export class LessonsPage {
     }
   ];
 }
+
